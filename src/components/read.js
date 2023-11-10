9381fb4bdcad11eb4a6a6e5e9d9ca696e7cd288d
Ryan Hogan <ryanoman2011@hotmail.com>
2023-11-10 14:45:24 +0000

    Changed the URL for the API.

--- src/components/read.js
+++ src/components/read.js
@@ -6,7 +6,7 @@ function Read(){
     const [data, setData] = useState([]); // Set to an empty array.
     useEffect(
         () => {
-            axios.get('https://jsonblob.com/api/jsonblob/1161593332966481920') // API URL
+            axios.get('http://localhost:4000/api/books') // API URL
             .then(
                 (response) => {
                     setData(response.data.books); // Update value.
